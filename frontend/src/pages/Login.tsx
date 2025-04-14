f19ad61a58d83973ed9c738c3601e65beed72a17
abhay-gits <anabhay42@gmail.com>
2025-04-14 14:45:27 +0530

    fix loginUI

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -36,50 +36,57 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center p-7 font-mono ">
-      <div className="rounded-2xl md:h-full w-full flex justify-center md:grid grid-cols-2 overflow-hidden">
-        <div className=" flex flex-col items-center justify-center p-10 ">
-          <h1 className="text-3xl font-bold mb-2">Tasks-Mates</h1>
+    <div className="h-screen flex items-center justify-center font-mono box-border">
+      <div className="min-h-full w-full grid grid-cols-1 md:grid-cols-2 gap-y-0">
+        {/* Left Hand Side */}
+        <div className="flex flex-col items-center justify-center p-10 md:p-30">
+          <h1 className="text-3xl font-bold mb-2 text-green-800">Tasks-Mates</h1>
           <p className="mb-5 text-center">Welcome back! Please enter your details.</p>
-          <form onSubmit={handleSubmit} className="flex flex-col w-full">
+          <form className="flex flex-col w-full" onSubmit={handleSubmit}>
             <label>Email</label>
             <input
-              name="email"
+            value={formData.email}
+            onChange={handleChange}
+            name="email"
+              required
               type="email"
-              className="border border-gray-400 rounded-md p-2 mb-4 w-full mt-2"
+              className="border border-green-400 rounded-md p-2 mb-4 w-full mt-2"
               placeholder="Enter your email"
-              value={formData.email}
-              onChange={handleChange}
             />
             <label>Password</label>
             <input
-              name="password"
               type="password"
-              className="border border-gray-400 rounded-md p-2 mb-4 w-full mt-2"
+              name="password"
+              required
+              className="border border-green-400 rounded-md p-2 mb-4 w-full mt-2"
               placeholder="**************"
               value={formData.password}
               onChange={handleChange}
             />
-            <button type="submit" className="bg-red-500 text-white rounded p-2">
+            <button className="bg-green-500 text-white rounded-md p-2 w-[80%] m-auto cursor-pointer hover:bg-green-600 transition duration-200">
               Sign In
             </button>
             <p className="text-center mt-4 text-sm md:text-base">
               Don't have an account?{" "}
-              <a href="/signup" className="text-blue-500 block md:inline">
+              <a href="/signup" className="text-green-500">
                 Sign Up
               </a>
             </p>
           </form>
         </div>
-        <div className="bg-gradient-to-br from-red-400 to-red-300 hidden md:flex items-center justify-center rounded-2xl ">
-          <img className="w-96 bg-contain" src={bg} />
+        {/* Right Hand Side */}
+        <div className="h-50 md:h-full p-6 order-1 md:order-2" >
+        <div className="bg-gradient-to-br from-green-400 to-green-300 flex items-center justify-center rounded-2xl h-full">
+          <img className="object-contain w-96 h-full" src={bg} />
         </div>
       </div>
+        </div>
     </div>
   )
 };
 
 export default Login;
+
 
 
 
